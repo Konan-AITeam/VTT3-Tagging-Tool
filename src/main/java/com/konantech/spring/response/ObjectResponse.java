eaacca74dd1a6d205c0c5a15444a2393cea4f490
yeonsikKonan <74032200+yeonsikKonan@users.noreply.github.com>
2020-11-10 16:12:54 +0900

    Update ObjectResponse.java

--- src/main/java/com/konantech/spring/response/ObjectResponse.java
+++ src/main/java/com/konantech/spring/response/ObjectResponse.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+
 @JsonIgnoreProperties({"headers", "statusCode", "statusCodeValue"})
 public class ObjectResponse<T> extends ResponseEntity<T> {
     public ObjectResponse() {
